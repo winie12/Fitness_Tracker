0b2a59f9f6157ed3743784f7d08cb50268fba8f5
winie12 <nse717355@gmail.com>
2024-09-13 13:59:27 -0600

    Final result

--- main.py
+++ main.py
@@ -40,25 +40,30 @@ class DaySelector:
 
 class SelectMuscleGroupsAndExercises(ABC):
     def __init__(self, split_name):
-        self.muscle_groups_by_day = {} 
+        self.dic_user_routine = {} 
         self.split_name = split_name
 
     def collect_muscle_groups_by_day(self, gym_days, predefined_groups = None):
-        print("\nFor each day, select the muscle group you want to train:")
         for i, day in enumerate(gym_days):
             if predefined_groups:
                 muscle_group = predefined_groups[i]
-            else:
-                muscle_group = input(f"What will you train on {day}? ").title()
-            exercises = self.collect_exercises(muscle_group)
-            self.muscle_groups_by_day[day] = {
-                'group': muscle_group,
+                exercises = self.collect_exercises(muscle_group)
+                self.dic_user_routine[day] = {
+                "group": muscle_group,
                 'exercises': exercises
-            }
+                }
+            else:
+                print("\nFor each day, select the muscle group you want to train.")
+                muscle_group = input(f"What will you train on {day}? ").title()
+                exercises = self.collect_exercises(muscle_group)
+                self.dic_user_routine[day] = {
+                "group": muscle_group,
+                'exercises': exercises
+                }
         self.display_schedule()
         self.write_to_csv()
         user_routine = WorkoutTracker(self.split_name)
-        user_routine.muscle_groups_by_day = self.muscle_groups_by_day
+        user_routine.dic_user_routine = self.dic_user_routine
         user_routine.register()
 
     def collect_exercises(self, muscle_group):
@@ -76,23 +81,21 @@ class SelectMuscleGroupsAndExercises(ABC):
         return exercises
 
     def display_schedule(self):
-        """Displays the training schedule."""
         print("\nYour training schedule:")
-        for day, details in self.muscle_groups_by_day.items():
+        for day, details in self.dic_user_routine.items():
             print(f"{day}: {details['group']} - Exercises: {', '.join(details['exercises'])}")
 
     def write_to_csv(self):
-        """Writes the training schedule to a CSV file."""
         filename = f"{self.split_name}.csv"
         if os.path.exists(filename):
             os.remove(filename)
         
         data = []
-        for day, details in self.muscle_groups_by_day.items():
+        for day, details in self.dic_user_routine.items():
             data.append({
-                'Day': day,
-                'Muscle Group': details['group'],
-                'Exercises': ', '.join(details['exercises'])
+                "Day": day,
+                "Muscle Group": details["group"],
+                "Exercises": ", ".join(details["exercises"])
             })
         df = pd.DataFrame(data)
         df.to_csv(filename, index=False)
@@ -104,7 +107,7 @@ class CreateSplit:
         self.gym_days = []  
 
     def create_custom_split(self):
-        """Creates a new custom split and prompts for weekly days."""
+        #Creates a new custom split and prompts for weekly days.
         new_split_name = input("Enter the name for the new split: ")  
         self.split_name = new_split_name
         print(f"Custom split '{new_split_name}' created.")
@@ -115,7 +118,7 @@ class CreateSplit:
         self.select_muscle_distribution()
 
     def select_muscle_distribution(self):
-        """Collects muscle groups and exercises for the selected days."""
+        #Collects muscle groups and exercises for the selected days.
         exercise_selector = SelectMuscleGroupsAndExercises(self.split_name)
         exercise_selector.collect_muscle_groups_by_day(self.gym_days)
 
@@ -130,7 +133,7 @@ class ArnoldSplit(SelectMuscleGroupsAndExercises):
 class PplSplit(SelectMuscleGroupsAndExercises):    
     def __init__(self):
         super().__init__("PPL")
-        self.predefined_groups = ["Chest, triceps and shoulders", "Back, biceps and forearms", "Leg", "Chest, triceps and shoulders", "Back, bicep and forearms", "Leg"]
+        self.predefined_groups = ["Chest, triceps and shoulders", "Back, biceps and forearms", "Leg", "Chest, triceps and shoulders", "Back, biceps and forearms", "Leg"]
     
     def collect_muscle_groups_by_day(self, gym_days):
        super().collect_muscle_groups_by_day(gym_days, predefined_groups = self.predefined_groups)
@@ -184,7 +187,7 @@ class AccessUserRoutine():
         if os.path.isfile(file):
             try:
                 user_routine = WorkoutTracker(User_routine_name)
-                user_routine.muscle_groups_by_day = self.load_routine(file)
+                user_routine.dic_user_routine = self.load_routine(file)
                 user_routine.register()
             except pd.errors.EmptyDataError:
                 print("The file is empty.")
@@ -217,63 +220,126 @@ class AccessUserRoutine():
 class WorkoutTracker(SelectMuscleGroupsAndExercises):
     def __init__(self, split_name):
         super().__init__(split_name)
-        self.muscle_groups_by_day = {}
+        self.dic_user_routine = {}
+        self.sorted_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
    
     
     def register(self):
         today_date = datetime.now().strftime("%Y-%m-%d")
         today_weekday = datetime.now().weekday()
-        today_day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][today_weekday]
+        today_day = self.sorted_days[today_weekday]
         
-        muscle_group = self.muscle_groups_by_day.get(today_day, {}).get("group", "rest")
+        muscle_group = self.dic_user_routine.get(today_day, {}).get("group", "rest")
         
-        if muscle_group == "rest":
-            print(f"Today is a rest day ")
-            return
-
-        print(f"Today muscle group is {muscle_group}")
-        user_register = input(f"Do you want to register todays routine ({today_date})? (yes/no): ").lower()
-        if user_register == "yes":
-            call_module(self.split_name)
-        else:
-            user_routines = input("Do you want to modify your routine? (yes/no): ").lower()
-            if user_routines == "yes":
-                self.modify_routine()
+        if muscle_group ==  "rest":
+            print(f"\nToday is rest day")
+            return_main_menu = MainMenu()
+            return return_main_menu.start()
+        else:   
+            print(f"Today is {muscle_group} day.")
+       
+            user_register = input(f"\nDo you want to register todays routine ({today_date})? (yes/no): ").lower()
+            if user_register == "yes":
+                try: 
+                    call_module(self.split_name, muscle_group)
+                except Exception as e:
+                    print(f"Error : {e}")
             else:
-                return_main_menu = MainMenu()
-                return return_main_menu.start()
+                user_routines = input("Do you want to modify your routine? (yes/no): ").lower()
+                if user_routines == "yes":
+                    self.modify_routine()
+                else:
+                    return_main_menu = MainMenu()
+                    return return_main_menu.start()
             
     def modify_routine(self): 
-        """Allows the user to modify the existing routine."""
-        if not self.muscle_groups_by_day:
+        if not self.dic_user_routine:
             print("No routine available to modify, add a new routine.")
             return_main_menu = MainMenu()
             return return_main_menu.start() 
-
-        print("\nCurrent routine:")
-        self.display_schedule()
-
-        while True: 
-            day_to_modify = input("Enter the day you want to modify: ").title()
-            if day_to_modify not in self.muscle_groups_by_day:
-                print(f"No routine found for {day_to_modify}.")
-            else:
-                new_muscle_group = input(f"Enter the new muscle group for {day_to_modify}: ").title()
-                new_exercises = self.collect_exercises(new_muscle_group)
-
-                self.muscle_groups_by_day[day_to_modify] = {
-                    'group': new_muscle_group,
-                    'exercises': new_exercises
-                }
-
-                self.write_to_csv()
-                self.display_schedule()
-                self.register()
-            break
-            
         
+        actions = ["add a day", "remove day", "modify a day"]
+        
+        for i,action in enumerate(actions, start=1):
+            print(f"{i}: {action}")
+            
+        while True:
+            try:
+                user_modify_option = int(input("\nIntroduce the number of the action you want to do: "))
+                if 1 <= user_modify_option <= 3: 
+                    break
+                else:
+                    print("please introduce a number bewtween 1-3")
+            except ValueError:
+                print("please introduce a valid number")
+        
+        #add day 
+        if user_modify_option == 1:
+            print("\nCurrent routine:")
+            self.display_schedule()
+            
+            while True:
+                add_day = DaySelector.get_valid_day()
+                if add_day in self.dic_user_routine:
+                    print("This day was already selected, please introduce other day.")
+                    continue
+                else:
+                    new_muscle_group = input(f"Enter the new muscle group for {add_day}: ").title()
+                    new_exercises = self.collect_exercises(new_muscle_group)
+
+                    self.dic_user_routine[add_day] = {
+                        'group': new_muscle_group,
+                        'exercises': new_exercises
+                    }
+                    self.write_to_csv()
+                    self.display_schedule()
+                    self.register()
+                break
+     
+        #remove day
+        elif user_modify_option == 2:
+            print("\nCurrent routine:")
+            self.display_schedule()
+
+            while True:
+                remove_days = [day.strip().title() for day in input("Enter the days you want to remove, separated by commas: ").split(",")]
+                if all(day in self.dic_user_routine for day in remove_days):
+                    for day in remove_days:
+                        if day in self.dic_user_routine:
+                            del self.dic_user_routine[day]
+
+                    self.write_to_csv()
+                    self.display_schedule()
+                    self.register()
+                    break
+                else:
+                    print(f"Some of the days you entered are not in the routine. Please check and try again.")
+
+        #modify a day 
+        elif user_modify_option == 3:
+            print("\nCurrent routine:")
+            self.display_schedule()
+
+            while True: 
+                day_to_modify = input("Enter the day you want to modify: ").title()
+                if day_to_modify not in self.dic_user_routine:
+                    print(f"No routine found for {day_to_modify}.")
+                else:
+                    new_muscle_group = input(f"Enter the new muscle group for {day_to_modify}: ").title()
+                    new_exercises = self.collect_exercises(new_muscle_group)
+
+                    self.dic_user_routine[day_to_modify] = {
+                        'group': new_muscle_group,
+                        'exercises': new_exercises
+                    }
+
+                    self.write_to_csv()
+                    self.display_schedule()
+                    self.register()
+                break
+            
+
         
 if __name__ == "__main__":
     menu = MainMenu()
     menu.start()
-
--- register/routine_register.py
+++ register/routine_register.py
@@ -3,112 +3,151 @@ import os
 from datetime import datetime
 
 def load_exercises(filename):
+    """Loads exercises from a CSV file."""
     try:
         df = pd.read_csv(filename)
         return df
     except FileNotFoundError:
-        print(f"El archivo '{filename}' no se encuentra.")
+        print(f"The file '{filename}' was not found.")
     except pd.errors.EmptyDataError:
-        print(f"El archivo '{filename}' está vacío.")
+        print(f"The file '{filename}' is empty.")
     except pd.errors.ParserError:
-        print(f"Error al analizar el archivo '{filename}'.")
+        print(f"Error parsing the file '{filename}'.")
     except Exception as e:
-        print(f"Ocurrió un error: {e}")
+        print(f"An error occurred: {e}")
 
 def get_exercises(dataframe):
-
-    exercises_dict = {}
+    exercise_dict = {}
     for _, row in dataframe.iterrows():
         day = row['Day']
-        exercises = row['Exercises'].split(', ') 
-        exercises_dict[day] = exercises
-    return exercises_dict
+        exercise_dict[day] = {
+        'group': row['Muscle Group'],
+        'exercises': row['Exercises'].split(', ')
+        }
+    return exercise_dict
+
 
 def workout_register(exercises_dict, filename):
     data = []
+    sorted_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+    today_weekday = datetime.now().weekday()
+    today_day = sorted_days[today_weekday]
     today_date = datetime.now().strftime("%Y-%m-%d")
     
+    muscle_group = exercises_dict.get(today_day, {}).get("group", "rest")
     
-    for exercise in exercises_dict.get(today_date, []):
-        print(f"\nRegistering data for the exercise {exercise} on {today_date}: ")
+    if muscle_group == "rest":
+        print(f"Today {today_date} is a rest day.")
+        return 
+    
+    if today_day in exercises_dict:
+        datos = exercises_dict[today_day]
+        exercises = datos["exercises"]
         
-        while True:
-            try:
-                sets = int(input("Number of sets: "))
-                if sets <= 0:
-                    print("Please enter a number greater than 0.")
-                    continue
-                break
-            except ValueError:
-                print("Please enter a valid number.")
+        print(f'Day: {today_day}')
+        print(f'Muscle group: {datos["group"]}')
         
-        for i in range(1, sets + 1):
+        for exercise in exercises:
+            print(f"What did you do for {exercise} on {today_date}?")
+            
             while True:
                 try:
-                    reps = int(input(f"How many reps did you do in set {i}?: "))
-                    if reps < 0:
+                    sets = int(input("Number of sets: "))
+                    if sets <= 0:
                         print("Please enter a number greater than 0.")
                         continue
-                    weight = float(input(f"Weight used in set {i} (kg): "))
-                    if weight < 0.5:
-                            print("Please enter a valid weight.")
-                            continue
                     break
                 except ValueError:
                     print("Please enter a valid number.")
             
-            data.append({
-                'Date': today_date,
-                'Exercise': exercise,
-                'Set': i,
-                'Reps': reps,
-                'Weight': weight
-            })
+            for i in range(1, sets + 1):
+                while True:
+                    try:
+                        weight = float(input(f"Weight used in set {i} (kg): "))
+                        if weight < 0.5:
+                            print("Please enter a valid weight.")
+                            continue
+                        reps = int(input(f"How many reps did you do in set {i}?: "))
+                        if reps < 0:
+                            print("Please enter a number greater than 0.")
+                            continue
+                        break
+                    except ValueError:
+                        print("Please enter a valid number.")
+                
+                data.append({
+                    'Date': today_date,
+                    'Exercise': exercise,
+                    'Set': i,
+                    'Reps': reps,
+                    'Weight': weight
+                })
     
     df = pd.DataFrame(data)
-    df.to_csv(filename, mode="a", header=True, index=False)
-    print(f"Data registered")
+    
+    if not os.path.isfile(filename):
+        df.to_csv(filename, mode="w", header=False, index=False)
+    else:
+        df.to_csv(filename, mode="a", header=False, index=False)
+    
+    print("Data registered successfully.")
 
 
-def User_stats(filename):
-    try:
-        df = pd.read_csv(filename)
+
+def user_stats(filename):
+    while True:
+        try:
+            df = pd.read_csv(filename)
         
-        if "Weight" not in df.columns or "Reps" not in df.columns:
-            print("El archivo no contiene las columnas necesarias.")
-            return
-    
-        stats = {
-            "max weight lift" : df["Weight"].max(),
-            "max reps made" : df["Reps"].max()
-        }
-    
-        print("\nThese are your current gym marks: ")
-        for key, value in stats.items():
-            print(f"{key}: {value} kg" if key == "max weight lift" else f"{key}: {value}" )
-    except FileExistsError:
-        print("File does not exist")
-    except pd.errors.EmptyDataError:
-        print("The file is empty")
-    except pd.errors.ParserError:
-        print("Can not parse file ")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+            if "Exercise" not in df.columns or "Weight" not in df.columns or "Reps" not in df.columns:
+                print("The file does not contain the required columns.")
+                break
 
+            exercise_stat = input("Introduce the exercise you want to check: ").lower()
+            df['Exercise'] = df['Exercise'].str.lower()
+
+            if exercise_stat not in df['Exercise'].values:
+                print(f"Cannot find {exercise_stat}. Please try again.")
+                continue
+
+            exercise_data = df[df['Exercise'] == exercise_stat]
+        
+            max_weight = exercise_data["Weight"].max()
+            max_reps = exercise_data[exercise_data["Weight"] == max_weight]["Reps"].max()
+
+            print("\nThese are your current gym marks for the exercise '{}':".format(exercise_stat))
+            print(f"Max weight lifted: {max_weight} kg")
+            print(f"Reps with max weight: {max_reps}")
+
+            break
+
+        except FileNotFoundError:
+            print("File does not exist.")
+            break
+        except pd.errors.EmptyDataError:
+            print("The file is empty.")
+            break
+        except pd.errors.ParserError:
+            print("Cannot parse file.")
+            break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
 
 
 def call_module(split_name, muscle_group):
-    
-    split_filename = f'/Users/nicolasdominguez/Desktop/Fitness_tracker_2.0/{split_name}.csv'
-    workouts_filename = f'{split_name}_workouts.csv'
+    workouts_filename = "/Users/nicolasdominguez/Desktop/Fitness_tracker_2.0/register/workouts.csv"
+    split_filename = f"/Users/nicolasdominguez/Desktop/Fitness_tracker_2.0/{split_name}.csv"
     split_data = load_exercises(split_filename)
-        
+    
     if split_data is not None:
-        exercises_dict = {day: details['exercises'] for day, details in split_data.items() if details['group'] == muscle_group}
+        exercises_dict = get_exercises(split_data)
         workout_register(exercises_dict, workouts_filename)
-        
-    User_gym_marks = input("Do you want to see your currect gym marks? (yes/no): ")
-    if User_gym_marks == "yes":
-        return User_stats(workouts_filename)
+    
+    user_gym_marks = input("Do you want to see your current gym marks? (yes/no): ").lower()
+    if user_gym_marks == "yes":
+        user_stats(workouts_filename)
     else:
-        return "Thanks for register your data."
+        print("Thanks for registering your data.")
+    
